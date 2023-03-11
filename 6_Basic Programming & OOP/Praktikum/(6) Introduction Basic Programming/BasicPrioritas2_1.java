b16db24e3c4863eec3c4414fe0234ecc4a8b1763
atikadewisetiani <95559118+atikasetiani@users.noreply.github.com>
2023-03-11 22:06:26 +0700

    Add files via upload

---
+++ 6_Basic Programming & OOP/Praktikum/(6) Introduction Basic Programming/BasicPrioritas2_1.java
@@ -0,0 +1,45 @@
+/*
+ * SECTION 6 - BASIC PROGRAMMING & OOP
+ * Introduction Basic Programming
+ * Praktikum Soal Prioritas 2 nomor 1
+ */
+package basicp.prioritas2_1;
+
+class BasicPrioritas2_1 {
+	public static void drawXYZ(int n) {
+            char[][] pattern = new char[n][n];
+            int x = 0, y = 0;
+
+            for (int i = 1; i <= n * n; i++) {
+		if (i % 2 == 1) {
+                    pattern[x][y] = 'Y';
+		} else if (i % 2 == 0) {
+                    pattern[x][y] = 'Z';
+		}
+
+		if (i % 3 == 0) {
+                    pattern[x][y] = 'X';
+		}
+
+		if (y == n - 1) {
+                    x++;
+                    y = 0;
+		} else {
+                    y++;
+		}
+            }
+
+            for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+                    System.out.print(pattern[i][j] + " ");
+		}
+                    System.out.println();
+            }
+	}
+
+	public static void main(String[] args) {
+            drawXYZ(5);
+	}
+}
+
+
