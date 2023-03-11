dc6f99aa931573d6c9832bddd42df1ba3c5f55f9
atikadewisetiani <95559118+atikasetiani@users.noreply.github.com>
2023-03-11 22:04:24 +0700

    Add files via upload

---
+++ 6_Basic Programming & OOP/Praktikum/(6) Introduction Basic Programming/BasicPrioritas1_1.java
@@ -0,0 +1,34 @@
+/*
+ * SECTION 6 - BASIC PROGRAMMING & OOP
+ * Introduction Basic Programming
+ * Praktikum Soal Prioritas 1 nomor 1
+ */
+package basicp.prioritas1_1;
+
+public class BasicPrioritas1_1 {
+   public static void main(String[] args) {
+       // segitiga
+       float alas = 20;
+       float tinggi = 25;
+
+       // hitung luas segitiga
+       float luasSegitiga = (alas * tinggi) / 2;
+       System.out.println("Luas segitiga adalah : " + luasSegitiga);
+
+       // persegi panjang
+       float panjang = 40;
+       float lebar = 6;
+
+       // hitung luas persegi panjang
+       float luasPersegiPanjang = panjang * lebar;
+       System.out.println("Luas persegi panjang adalah : " + luasPersegiPanjang);
+
+       // lingkaran
+       double jari2 = 7;
+
+       // hitung luas lingkaran
+       double luasLingkaran = Math.PI * Math.pow(jari2, 2);
+       System.out.println("Luas lingkaran adalah: " + luasLingkaran);
+   }
+}
+
